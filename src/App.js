89a0fde5f3c9c7de829058ae5f8a87520878c329
Gregory Diaz <gregdiaz8624@yahoo.com>
2020-04-06 11:29:01 -0400

    MVP

--- src/App.js
+++ src/App.js
@@ -8,13 +8,48 @@ import SearchBar from './Components/SearchBar'
 
 class App extends React.Component {
 
+  state = {
+    planeteers: [],
+    search: ""
+  }
+
+  changeSearch = (inputFromChild) => {
+    this.setState({
+      search: inputFromChild
+    })
+  }
+
+  componentDidMount(){
+
+    fetch("http://localhost:4000/planeteers")
+      .then(r => r.json())
+      .then(planetters => {
+            this.setState({planeteers: planetters}) 
+      })
+  }
+
+  returnsArray = () => {
+    let {planeteers, search} = this.state
+    let filteredPlaneteers = planeteers.filter((planeteer)=>{
+      return planeteer.name.toLowerCase().includes(search.toLowerCase()) || planeteer.bio.toLowerCase().includes(search.toLowerCase())
+    })
+      return filteredPlaneteers
+    }
+
+  addANewPlaneteerToDom = (newPlaneteer) => {
+    let newListofPlaneteers = [...this.state.planeteers, newPlaneteer]
+    this.setState({
+      planeteers: newListofPlaneteers
+    })
+  }
+
   render(){
     return (
       <div>
         <Header />
-        <SearchBar />
-        <RandomButton/>
-        <PlaneteersContainer />
+        <SearchBar  search={this.state.search}  changeSearch={this.changeSearch}          />
+        <RandomButton addANewPlaneteerToDom={this.addANewPlaneteerToDom}/>
+        <PlaneteersContainer planeteers={this.returnsArray()}/>
       </div>
     );
   }
--- src/Components/Planeteer.jsx
+++ src/Components/Planeteer.jsx
@@ -2,18 +2,34 @@ import React from 'react';
 
 class Planeteer extends React.Component {
 
+  state={
+    haveIBeenClicked: false
+  }
+
+  handleClick=(e) => {
+    
+    this.setState({
+      haveIBeenClicked: !this.state.haveIBeenClicked
+      })
+  }
+
   render() {
+
+    let planeteer = this.props.planeteer
+    let {name, fromUSA, born, bio, quote, pictureUrl, twitter} = planeteer
+    let age = new Date().getFullYear() - born
+    // console.log(this.props.planeteer)
     return (
       <li className="cards__item">
-        <div className="card">
-          <img src={"RENDER IMAGE"} alt={"RENDER PERSON NAME"} className="card__image" />
+        <div className="card" onClick={this.handleClick}>
+          <img src={pictureUrl} alt={name} className="card__image" />
           <div className="card__content">
-            <div className="card__title">{"RENDER NAME"}</div>
-            <p className="card__text">{"CONDITIONALLY RENDER BIO OR QUOTE"}</p>
+            <div className="card__title">{name}</div>
+            <p className="card__text">{ this.state.haveIBeenClicked ? quote : bio }</p>
             <div className="card__detail">
-              <p>{"RENDER TWITTER HANDLE"}</p>
-              <p>Age: {"RENDER THE AGE OF THE PERSON"}</p>
-              <p>{"CONDITIONALLY RENDER WHETHER THE PERSON IS USA-BASED OR WORKING OVERSEAS"}</p>
+              <p>{twitter}</p>
+              <p>Age: {age}</p>
+              <p>{ fromUSA ? "USA-BASED" : "WORKING OVERSEAS" }</p>
             </div>
             {/* DELIVERABLE 5 */}
           </div>
