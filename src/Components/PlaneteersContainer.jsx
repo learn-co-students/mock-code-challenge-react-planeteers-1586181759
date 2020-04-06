3ed4628e4da7c85ee267b9b3636057398cfc4720
Alyssa Clarke <alyssa.clarke00@gmail.com>
2020-04-06 11:33:13 -0400

    done

--- src/Components/PlaneteersContainer.jsx
+++ src/Components/PlaneteersContainer.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Planeteer from './Planeteer'
 
-const PlaneteersContainer = () => {
+const PlaneteersContainer = (props) => {
+
+  let {planeteers} = props
+
+  let renderPlaneteers = planeteers.map((planeteer) => {
+    return <Planeteer key={planeteer.id} planeteer={planeteer} />
+  })
 
   return (
     <ul className="cards">
       {
-        "Render Planeteers here"
+        renderPlaneteers
       }
     </ul>
   )
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
+  handleChange = (e) => {
+    this.props.changeSearchTerm(e.target.value)
+  }
+
+
   render() {
+ 
     return (
       <div className="search">
-       <input type="text" className="searchTerm" placeholder="Who would you like to search for?"/>
+       <input type="text" 
+              className="searchTerm" 
+              placeholder="Who would you like to search for?"
+              value={this.props.searchTerm}
+              onChange={this.handleChange} />
       </div>
     );
   }
